541aad71a1264cd9657595353c646175da05153f
huanghaibin <huanghaibin_dev@163.com>
2017-08-29 17:11:15 +0800

    修正摇一摇UI以及去掉限制

--- app/src/main/java/net/oschina/app/improve/main/discover/ShakePresentFragment.java
+++ app/src/main/java/net/oschina/app/improve/main/discover/ShakePresentFragment.java
@@ -1,5 +1,6 @@
 package net.oschina.app.improve.main.discover;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -27,14 +28,13 @@ import java.lang.reflect.Type;
  * 摇一摇礼品相关实现
  */
 public class ShakePresentFragment extends BaseSensorFragment<ShakePresent> {
-    private Button mBtnShakeAgain, mBtnGet;
+    private Button mBtnShakeAgain;
     private ImageView mImgPig;
     private TextView mTxtName;
     private boolean mCanAgain;
 
     public static ShakePresentFragment newInstance() {
-        ShakePresentFragment fragment = new ShakePresentFragment();
-        return fragment;
+        return new ShakePresentFragment();
     }
 
     @Override
@@ -50,12 +50,13 @@ public class ShakePresentFragment extends BaseSensorFragment<ShakePresent> {
         return R.layout.fragment_shake_present;
     }
 
+    @SuppressLint("InflateParams")
     @Override
     protected void initWidget(View root) {
         super.initWidget(root);
         mShakeView = mInflater.inflate(R.layout.view_present, null);
         mBtnShakeAgain = (Button) mShakeView.findViewById(R.id.btn_shake_again);
-        mBtnGet = (Button) mShakeView.findViewById(R.id.btn_get);
+        Button mBtnGet = (Button) mShakeView.findViewById(R.id.btn_get);
         mImgPig = (ImageView) mShakeView.findViewById(R.id.iv_pig);
         mTxtName = (TextView) mShakeView.findViewById(R.id.tv_name);
         mBtnShakeAgain.setOnClickListener(this);
@@ -69,7 +70,7 @@ public class ShakePresentFragment extends BaseSensorFragment<ShakePresent> {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_shake_again:
-                if (mLoading && mCanAgain) {
+                if (!mLoading && mCanAgain) {
                     mCardView.clearAnimation();
                     mCardView.setVisibility(View.GONE);
                     mCardView.removeAllViews();
@@ -143,7 +144,7 @@ public class ShakePresentFragment extends BaseSensorFragment<ShakePresent> {
                     }
                 }, 1000);
             } else {
-                mTvState.setText(mBean != null ? mBean.getMessage() : "很抱歉，出现未知错误");
+                mTvState.setText(mBean != null ? mBean.getMessage() : "没有摇到礼品");
                 mLoading = false;
             }
         }
@@ -156,6 +157,8 @@ public class ShakePresentFragment extends BaseSensorFragment<ShakePresent> {
         getImgLoader().load(present.getPic()).placeholder(R.mipmap.ic_split_graph).into(mImgPig);
         mTxtName.setText(present.getName());
         mTvState.setText("恭喜您中奖了");
+        mLoading = false;
+        mCanAgain = true;
     }
 
     @Override
