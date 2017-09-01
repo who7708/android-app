2612419b4e5ebae676ab53579c2386d0f68cd9cc
huanghaibin <huanghaibin_dev@163.com>
2017-09-01 17:41:41 +0800

    去除H4

--- app/src/main/java/net/oschina/app/improve/write/HPopupWindow.java
+++ app/src/main/java/net/oschina/app/improve/write/HPopupWindow.java
@@ -4,11 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
 import net.oschina.app.R;
 import net.oschina.app.improve.widget.rich.TextSection;
@@ -20,7 +21,7 @@ import net.oschina.app.improve.widget.rich.TextSection;
 
 class HPopupWindow extends PopupWindow implements View.OnClickListener {
     private OnHeaderChangeListener mListener;
-    private TextView mTextH1, mTextH2, mTextH3, mTextH4;
+    private ImageView mImageH1, mImageH2, mImageH3;
 
     @SuppressLint("InflateParams")
     HPopupWindow(Context context, OnHeaderChangeListener listener) {
@@ -32,34 +33,29 @@ class HPopupWindow extends PopupWindow implements View.OnClickListener {
         setFocusable(false);
         this.mListener = listener;
         View content = getContentView();
-        mTextH1 = (TextView) content.findViewById(R.id.tv_h1);
-        mTextH2 = (TextView) content.findViewById(R.id.tv_h2);
-        mTextH3 = (TextView) content.findViewById(R.id.tv_h3);
-        mTextH4 = (TextView) content.findViewById(R.id.tv_h4);
 
-        mTextH1.setOnClickListener(this);
-        mTextH2.setOnClickListener(this);
-        mTextH3.setOnClickListener(this);
-        mTextH4.setOnClickListener(this);
+        mImageH1 = (ImageView) content.findViewById(R.id.iv_h1);
+        mImageH2 = (ImageView) content.findViewById(R.id.iv_h2);
+        mImageH3 = (ImageView) content.findViewById(R.id.iv_h3);
+
+        mImageH1.setOnClickListener(this);
+        mImageH2.setOnClickListener(this);
+        mImageH3.setOnClickListener(this);
     }
 
     void setStyle(TextSection section) {
-        mTextH1.setTextColor(0xFFFFFFFF);
-        mTextH2.setTextColor(0xFFFFFFFF);
-        mTextH3.setTextColor(0xFFFFFFFF);
-        mTextH4.setTextColor(0xFFFFFFFF);
+        DrawableCompat.setTint(mImageH1.getDrawable(), 0xFFFFFFFF);
+        DrawableCompat.setTint(mImageH2.getDrawable(), 0xFFFFFFFF);
+        DrawableCompat.setTint(mImageH3.getDrawable(), 0xFFFFFFFF);
         switch (section.getTextSize()) {
             case 28:
-                mTextH1.setTextColor(0xff24cf5f);
+                DrawableCompat.setTint(mImageH1.getDrawable(), 0xff24cf5f);
                 break;
             case 24:
-                mTextH2.setTextColor(0xff24cf5f);
+                DrawableCompat.setTint(mImageH2.getDrawable(), 0xff24cf5f);
                 break;
             case 20:
-                mTextH3.setTextColor(0xff24cf5f);
-                break;
-            case 16:
-                mTextH4.setTextColor(0xff24cf5f);
+                DrawableCompat.setTint(mImageH3.getDrawable(), 0xff24cf5f);
                 break;
         }
     }
@@ -69,29 +65,22 @@ class HPopupWindow extends PopupWindow implements View.OnClickListener {
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.tv_h1:
-                mTextH1.setSelected(!mTextH1.isSelected());
-                mListener.onTitleChange(mTextH1.isSelected() ? 28 : 18);
-                mTextH1.setTextColor(mTextH1.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
-                break;
-            case R.id.tv_h2:
-                mTextH2.setSelected(!mTextH2.isSelected());
-                mListener.onTitleChange(mTextH2.isSelected() ? 24 : 18);
-                mTextH2.setTextColor(mTextH2.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
+            case R.id.iv_h1:
+                mImageH1.setSelected(!mImageH1.isSelected());
+                mListener.onTitleChange(mImageH1.isSelected() ? 28 : 18);
+                DrawableCompat.setTint(mImageH1.getDrawable(), mImageH1.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
 
                 break;
-            case R.id.tv_h3:
-                mTextH3.setSelected(!mTextH3.isSelected());
-                mListener.onTitleChange(mTextH3.isSelected() ? 20 : 18);
-                mTextH3.setTextColor(mTextH3.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
+            case R.id.iv_h2:
+                mImageH2.setSelected(!mImageH2.isSelected());
+                mListener.onTitleChange(mImageH2.isSelected() ? 24 : 18);
+                DrawableCompat.setTint(mImageH2.getDrawable(), mImageH2.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
 
                 break;
-            case R.id.tv_h4:
-
-                mTextH4.setSelected(!mTextH4.isSelected());
-                mListener.onTitleChange(mTextH4.isSelected() ? 16 : 18);
-                mTextH4.setTextColor(mTextH4.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
-
+            case R.id.iv_h3:
+                mImageH3.setSelected(!mImageH3.isSelected());
+                mListener.onTitleChange(mImageH3.isSelected() ? 20 : 18);
+                DrawableCompat.setTint(mImageH3.getDrawable(), mImageH3.isSelected() ? 0xff24cf5f : 0xFFFFFFFF);
                 break;
         }
     }
