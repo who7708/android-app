dfec1215e87b164e7bef77e30fde8196561403f9
huanghaibin <huanghaibin_dev@163.com>
2017-12-11 16:05:55 +0800

    完成系统分享

--- app/src/main/java/net/oschina/app/improve/bean/base/ResultBean.java
+++ app/src/main/java/net/oschina/app/improve/bean/base/ResultBean.java
@@ -1,9 +1,5 @@
 package net.oschina.app.improve.bean.base;
 
-import android.util.Log;
-
-import com.google.gson.Gson;
-
 import net.oschina.app.improve.notice.NoticeBean;
 import net.oschina.app.improve.notice.NoticeManager;
 
@@ -65,9 +61,6 @@ public class ResultBean<T> {
 
     public boolean isSuccess() {
         // 每次回来后通知消息到达
-        if(notice!= null){
-            Log.e("notice",new Gson().toJson(notice));
-        }
         NoticeManager.publish(this, this.notice);
         return code == RESULT_SUCCESS && result != null;
     }
