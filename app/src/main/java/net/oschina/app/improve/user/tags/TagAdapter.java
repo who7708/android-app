9c161c1700ec041c69fd82f209ec65448f48bb3c
huanghaibin_dev@163.com <huanghaibin_dev@163.com>
2018-05-30 22:51:31 +0800

     标签删除

---
+++ app/src/main/java/net/oschina/app/improve/user/tags/TagAdapter.java
@@ -0,0 +1,38 @@
+package net.oschina.app.improve.user.tags;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.oschina.app.R;
+import net.oschina.app.improve.base.adapter.BaseRecyclerAdapter;
+import net.oschina.app.improve.bean.Tags;
+
+public class TagAdapter extends BaseRecyclerAdapter<Tags> {
+    public TagAdapter(Context context) {
+        super(context, ONLY_FOOTER);
+    }
+
+    @Override
+    protected RecyclerView.ViewHolder onCreateDefaultViewHolder(ViewGroup parent, int type) {
+        return new TagHolder(mInflater.inflate(R.layout.item_list_tag,parent,false));
+    }
+
+    @Override
+    protected void onBindDefaultViewHolder(RecyclerView.ViewHolder holder, Tags item, int position) {
+        TagHolder h = (TagHolder)holder;
+        h.mTextTag.setText(item.getName());
+    }
+
+    private static final class TagHolder extends RecyclerView.ViewHolder{
+        private TextView mTextTag;
+        private TextView mTextDelete;
+        private TagHolder(View itemView) {
+            super(itemView);
+            mTextTag = (TextView)itemView.findViewById(R.id.tv_tag);
+            mTextDelete = (TextView)itemView.findViewById(R.id.tv_delete);
+        }
+    }
+}
